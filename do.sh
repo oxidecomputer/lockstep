77b1752e2c90bfc387f0dfa7c35a1729a436bd46
James MacMahon <james@oxide.computer>
2023-06-27 10:22:21 -0400

    do.sh

---
+++ do.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ex
+
+rm -rf staging/ || true
+mkdir staging/
+cd staging
+
+git clone --depth 1 --branch main git@github.com:oxidecomputer/omicron
+git clone --depth 1 --branch main git@github.com:oxidecomputer/crucible
+git clone --depth 1 --branch master git@github.com:oxidecomputer/propolis
+git clone --depth 1 --branch main git@github.com:oxidecomputer/dendrite
+git clone --depth 1 --branch main git@github.com:oxidecomputer/maghemite
+
+../target/debug/lockstep
+
